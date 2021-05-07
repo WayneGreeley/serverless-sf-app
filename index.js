bb6fbf17fe5a35841def8e4edf44dab249fb2d98
Wayne Greeley <it199@hotmail.com>
2021-05-06 22:11:03 -0500

    remove console logs

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const AWS = require('aws-sdk')
 const s3 = new AWS.S3()
-const uuid = require('node-uuid')
 
 function getRandomIntInclusive(min, max) {
     const mini = Math.ceil(min)
@@ -10,69 +9,45 @@ function getRandomIntInclusive(min, max) {
 
 
 module.exports.validate = async (event) => {
-    console.log(`event:${JSON.stringify(event)}`)
-    const data = event.data
-    const key = uuid.v4()
-    const newData = {...data, coverage_valid: true, key: key}
-    console.log("newData:"+newData)
-    return newData
+    return {...event, coverage_valid: true}
 }
 
 module.exports.getoptions = async (event) => {
-    console.log(`event:${JSON.stringify(event)}`)
-
     const outputOptions = []
     for (let i = 0; i < 10; i++ ){
         outputOptions.push(getRandomIntInclusive(1, 99))
     }
-    console.log("outputOptions:"+outputOptions)
-    return outputOptions
+    return {...event, outputOptions: outputOptions}
 }
 
 module.exports.recommend = async (event) => {
-    console.log(`event:${JSON.stringify(event)}`)
-
     const recommended = getRandomIntInclusive(30, 50)
-    console.log("recommended:"+recommended)
-    return recommended
+    return {...event, recommended: recommended}
 }
 
 module.exports.choose = async (event) => {
-    console.log(`event:${JSON.stringify(event)}`)
-
-    const options = event.outputOptions
-    const recommended = event.recommended
-
     let closestDistance = 99999
     let closestValue = 0
-    for (option in options){
-        let distance = option - recommended
+
+    event.outputOptions.forEach(num => {
+        let distance = event.recommended - num
         if (distance >= 0 && distance < closestDistance){
             closestDistance = distance
-            closestValue = option
+            closestValue = num
         }
-    }
-
-    return closestValue
+    })
+    return {...event, closestValue: closestValue}
 }
 
 module.exports.calc = async (event) => {
-    console.log(`event:${JSON.stringify(event)}`)
-    const closestValue = event.closestValue
-
-    const quote = closestValue * 3.14
-    console.log("quote:"+quote)
-    return quote
+    const quote = event.closestValue * 3.14
+    return {...event, quote: quote}
 }
 
 module.exports.save = async (event) => {
-    console.log(`event:${JSON.stringify(event)}`)
-
     const s3Response = await s3.putObject({
         Bucket: process.env.BUCKET,
-        Key: event.key,
-        Body: event.data,
+        Key: event.key+".txt",
+        Body: JSON.stringify(event),
     }).promise()
 }
-
-
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
     entry: slsw.lib.entries,
+    mode: 'development',
     target: 'node',
     // Generate sourcemaps for proper error messages
     devtool: 'source-map',
